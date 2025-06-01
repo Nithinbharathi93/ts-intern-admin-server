a56c25a77461887704252411cdba5244d1fe2d10
Nithinbharathi93 <nithinthelordest@gmail.com>
2025-06-01 21:56:31 +0530

    crucial change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 
 
 const sheetId = process.env.SHEET_ID;
